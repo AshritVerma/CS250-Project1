599f51e9512b6b88a5713a37f16f64ed3e13d368
ginnsato <ginnsato@gmail.com>
2022-10-22 17:21:25 +0200

    third commit

--- common/progargs.cpp
+++ common/progargs.cpp
@@ -2,16 +2,32 @@
 #include <vector>
 #include <fstream>
 #include <filesystem>
+
 #include "../common/progargs.hpp"
 
 using namespace std;
 
-int is_bmp_valid(string s){
+using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
+
+void get_dimensions(string name, int &start, int &width, int &height, int &padding){
+    unsigned char header[54];               // define header
+    FILE *fp = fopen(name.c_str(), "rb");   // open file
+
+    fread(header, sizeof(header), 1, fp);   // read header
+    fclose(fp);
+
+    // calculate the following values
+    start = header[10] + (header[11] << 8) + (header[12] << 16) + (header[13] << 24);
+    width = header[18] + (header[19] << 8) + (header[20] << 16) + (header[21] << 24);
+    height = header[22] + (header[23] << 8) + (header[24] << 16) + (header[25] << 24);
+    padding = ((4 - (width * 3) % 4) % 4);
+
+}
+
+int is_bmp_valid(string name){
 
     unsigned char header[54];
-    const char * b = s.c_str();
-
-    FILE *fp = fopen(b, "rb");                                  // open file
+    FILE *fp = fopen(name.c_str(), "rb");                       // open file
     fread(header, sizeof(header), 1, fp);                       // read file 1 byte at a time and store in header array
     fclose(fp);                                                 // done using file here
 
@@ -29,49 +45,62 @@ void copy(string src, string dst){
     std::filesystem::copy(src, dst);
 }
 
-void histo(string name){
-    int r_vals[256], g_vals[256], b_vals[256];          // array of values to store counts in
-    for(int r=0; r < 256; r++){
-        r_vals[r] = 0;
-        g_vals[r] = 0;
-        b_vals[r] = 0;
-    }
+void write_array(ofstream &hst, int arr[256]){
+    for(int i = 0; i < 256; i++)
+        hst << arr[i] << endl;
+}
 
-    unsigned char header[54];
-    const char * b = name.c_str();
 
-    FILE *fp = fopen(b, "rb");              // open file
-    fread(header, sizeof(header), 1, fp);
-
-    int start_val = header[10] + (header[11] << 8) + (header[12] << 16) + (header[13] << 24);
-    fseek(fp,start_val,0);
-
-    int width = header[18] + (header[19] << 8) + (header[20] << 16) + (header[21] << 24);
-    int height = header[22] + (header[23] << 8) + (header[24] << 16) + (header[25] << 24);
-    
-    int padding = ((4 - (width * 3) % 4) % 4);
-
+// 24 lines without any blank lines
+void histo(string name, string src, string dst){
+    int r_vals[256], g_vals[256], b_vals[256], start, width, height, padding;
+    unsigned char c[3];
+    for(int r=0; r < 256; r++)
+        r_vals[r] = g_vals[r] = b_vals[r] = 0;
+    get_dimensions(name, start, width, height, padding);
+    FILE *fp = fopen(name.c_str(), "rb");
+    fseek(fp, start, 0);
     for(int y = 0; y < height; y++){
         for(int x = 0; x < width; x++){
-            unsigned char c[3];
             fread(c, sizeof(c), 1, fp);         // read color's rgb values
-            b_vals[int(c[0])]++;                     // iterate value 
+            b_vals[c[0]]++;                     // iterate value 
             g_vals[c[1]]++;                     
             r_vals[c[2]]++; 
             fseek(fp,3,SEEK_CUR);               // move fp
         }
         fseek(fp,padding,SEEK_CUR);             // skip padding
     }
-    int sum = 0;
-    for(int i = 0; i < 256; i++){
-        sum += b_vals[i] + g_vals[i] + r_vals[i];
-    }
-    cout << sum << endl;
-    cout << width * height << endl;
     fclose(fp);
+
+    ofstream hst(dst + "/" + name.substr(src.size()+1,name.size()-src.size()-4)+"hst");                     // create ofstream called hst
+
+    write_array(hst, r_vals);
+    write_array(hst, g_vals);
+    write_array(hst, b_vals);   
+
+    hst.close();
+
 }
 
 
+/*
+int main(){
+    histo("images/balloon.bmp", "images", "test_out");
+
+    
+    vector <string> files;
+
+
+    for (const auto& dirEntry : recursive_directory_iterator("images"))
+        files.push_back(dirEntry.path());
+
+    for(int i=0; i < files.size(); ++i){
+        cout << files[i] << endl;
+    }
+    return 0;
+    
+} 
+*/
 
 
 
--- common/progargs.hpp
+++ common/progargs.hpp
@@ -4,7 +4,15 @@
 #include <filesystem>
 
 
+
+void get_dimensions(std::string name, int &start, int &width, int &height, int &padding); 
+
 int is_bmp_valid(std::string s);
 
 void copy(std::string src, std::string dst);
+
+void write_array(std::ofstream &hst, int arr[256]);
+
+void histo(std::string name, std::string src, std::string dst);
+
 
